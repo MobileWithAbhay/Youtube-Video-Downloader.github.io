7a3df1d1df0717cc6375815641f236714a4af338
MobileWithAbhay <116576549+MobileWithAbhay@users.noreply.github.com>
2022-10-25 13:29:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var link = document.querySelector('.link');
+var format = document.querySelector('.format');
+
+function download() {
+  if(link.value!=""){
+   if(link.value.indexOf("https://youtu.be/") != -1)
+     {
+   var url1 = link.value.replace("https://youtu.be/","https://www.youtube.com/embed/");
+  
+  document.querySelector('.result1').innerHTML = `<iframe width="100%" height="190" src="${url1}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+
+  var url2 = `https://loader.to/api/button/?url=${link.value}&f=${format.value}`;
+  
+  document.querySelector('.result2').innerHTML = `<iframe style="width:100%;height:60px;border:hidden;overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
+  }
+  else if(link.value.indexOf("https://www.youtube.com/watch?v=") != -1)
+      {
+  var url1 = link.value.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/");
+  
+  document.querySelector('.result1').innerHTML = `<iframe width="100%" height="190" src="${url1}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+  var u = link.value.replace("https://www.youtube.com/watch?v=","https://youtu.be/");
+  var url2 = `https://loader.to/api/button/?url=${u}&f=${format.value}`;
+  
+  document.querySelector('.result2').innerHTML = `<iframe style="width:100%;height:60px;border:hidden;overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
+      }
+   }
+}
