6c3c9b4d1c0c87fe41554b7430c32b50c1d29a85
MobileWithAbhay <116576549+MobileWithAbhay@users.noreply.github.com>
2022-11-25 08:57:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,5 +25,17 @@ function download() {
   
   document.querySelector('.result2').innerHTML = `<iframe style="width:100%;height:60px;border:hidden;overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
       }
+      else if (link.value.indexOf("https://youtube.com/shorts/") != -1)
+     {
+       var url1 = link.value.replace("https://youtube.com/shorts/", "https://www.youtube.com/embed/");
+       var u3 = url1.replace("?feature=share", "");
+       document.querySelector('.result1').innerHTML = `<iframe width="100%" height="190" src="${u3}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+     
+       var u = link.value.replace("https://youtube.com/shorts/", "https://youtu.be/");
+       var u2 = u.replace("?feature=share", "");
+       var url2 = `https://loader.to/api/button/?url=${u2}&f=${format.value}`;
+     
+       document.querySelector('.result2').innerHTML = `<iframe style="width:100%;height:60px;border:hidden;overflow:hidden;" scrolling="no" src="${url2}"></iframe>`;
+     }
    }
 }
